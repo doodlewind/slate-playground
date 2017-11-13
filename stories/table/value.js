e10e776b43243f2b1db3c80f96b9ad1e771af8c7
Yifeng Wang <i@ewind.us>
2017-11-13 20:18:03 +0800

    feat: handle batch highlight

--- stories/table/value.js
+++ stories/table/value.js
@@ -18,46 +18,46 @@ const value = (
       </paragraph>
       <table type='paragraph'>
         <table_row>
-          <table_cell>
+          <table_cell col_span={1} row_span={1}>
             0, 0
           </table_cell>
-          <table_cell>
+          <table_cell col_span={1} row_span={1}>
             0, 1
           </table_cell>
-          <table_cell>
+          <table_cell col_span={1} row_span={1}>
             0, 2
           </table_cell>
-          <table_cell>
+          <table_cell col_span={1} row_span={1}>
             0, 3
           </table_cell>
         </table_row>
 
         <table_row>
-          <table_cell>
+          <table_cell col_span={1} row_span={1}>
             1, 0
           </table_cell>
-          <table_cell>
+          <table_cell col_span={1} row_span={1}>
             1, 1
           </table_cell>
-          <table_cell>
+          <table_cell col_span={1} row_span={1}>
             1, 2
           </table_cell>
-          <table_cell>
+          <table_cell col_span={1} row_span={1}>
             1, 3
           </table_cell>
         </table_row>
 
         <table_row>
-          <table_cell>
+          <table_cell col_span={1} row_span={1}>
             2, 0
           </table_cell>
-          <table_cell>
+          <table_cell col_span={1} row_span={1}>
             2, 1
           </table_cell>
-          <table_cell>
+          <table_cell col_span={1} row_span={1}>
             2, 2
           </table_cell>
-          <table_cell>
+          <table_cell col_span={1} row_span={1}>
             2, 3
           </table_cell>
         </table_row>
